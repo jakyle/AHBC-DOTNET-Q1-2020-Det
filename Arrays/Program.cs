b87afcd29d14348945ee7bf3aa176783f67db509
Jackson, James <jamesjackson@quickenloans.com>
2020-01-30 21:31:06 -0500

    arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            //                                0  1  2  3  4  
+            int[] otherNumbers = new int[] { 13, 0, 5, 1, 2 };
+
+            // int indexSeven = otherNumbers[7];
+
+            int thirdNumberInArray = otherNumbers[2];
+
+            int amountOfEvenNumbers = GetAmountOfEvenNumbers(otherNumbers);
+            int numbersIsEven = GetAmountOfEvenNumbers(numbers);
+            
+            //
+            //
+            int amountOfVowels = GetVowelsInWords(new string[] { "Hello", "is", "there", "anybody", "out", "there"});
+
+            string[] names = new string[] { "james", "jimmy", "larry" };
+
+            int[][] nestedNumbersRedux = new[]
+            {
+                //   j   0  1  2  3          i
+                new [] { 7, 2, 3, 4},     // 0 
+                new [] { 13, 14, 99, 2},  // 1
+                new [] { 33, 24, 3, 4}      // 2
+            };
+
+            int[][] nestedNumbers = new[]
+            {
+                //   j   0  1  2  3          i
+                new [] { 1, 2, 3, 4},     // 0 
+                new [] { 13, 14, 99, 2},  // 1
+                new [] { 8, 2, 3, 4}      // 2
+            };
+
+            GetOddNumbers(nestedNumbers);
+        }
+
+        static int GetAmountOfEvenNumbers(int[] numbers)
+        {
+            int accumulator = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    accumulator++;
+                }
+            }
+
+            return accumulator;
+        }
+        //  numbers = nestedNumbers
+        static int GetOddNumbers(int[][] numbers)
+        {
+            int accumulator = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers[i].Length; j++)
+                {
+                    if (numbers[i][j] % 2 != 0) 
+                    {
+                        accumulator++;
+                    }
+                }
+            }
+
+            return accumulator;
+        }
+
+        static int GetVowelsInWords(string[] words)
+        {
+            int accumulator = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    // words[i] == "hello"
+                    switch (words[i].ToLower()[j])
+                    {
+                        case 'a':
+                        case 'e':
+                        case 'i':
+                        case 'o':
+                        case 'u':
+                            accumulator++;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
--- Functions/Program.cs
+++ Functions/Program.cs
@@ -5,6 +5,55 @@ namespace Functions
 {
     class Program
     {
+        
+
+        static void Main(string[] args)
+        {
+            // Functions
+            double areaOfMyRoom = FindArea(11.2, 23.6);
+            double areaOfDarionsRoom = FindArea(69.2, 420);
+            double volumeOfMyRoom = FindVolume(33.2, 56.1, 22.4);
+            double myLivingRoom = FindArea(22.2, 33.1);
+
+
+            double valueOne = 10.0;
+            double valueTwo = FindArea(5, 2);
+
+            //                                  0             1                2
+            string[] ourNumbers = new[] { "123-123-1234", "321-321-4321", "sup dude" };
+            
+            for (int i = 0; i < ourNumbers.Length; i++)
+            {
+                bool isValidPhoneNumber = IsValidPhoneNumber(ourNumbers[i]);
+                if (isValidPhoneNumber)
+                {
+                    Console.WriteLine("Hit me up dude");
+                }
+                else
+                {
+                   Console.WriteLine("Invalid Phone Number");
+                }
+            }
+
+            PrintHello();
+            PrintMessage("Sup dude");
+            PrintMessage("hello", "how are you");
+
+            Console.WriteLine('a');
+
+            int userInput = GetValidatedUserInt();
+
+            string inputtedNumber = "123-123-1234";
+            if (TryParsePhoneNumber(inputtedNumber, out string validPhoneNumber))
+            {
+                validPhoneNumber
+            }
+            else
+            {
+
+            }
+        }
+
         static int AddTwoNumbers(int numOne, int numTwo)
         {
             int summedResult = numOne + numTwo;
@@ -16,31 +65,6 @@ namespace Functions
         {
             double area = length * width;
             return area;
-        }
-
-        static void Main(string[] args)
-        {
-            // Functions
-            double areaOfMyRoom = FindArea(11.2, 23.6);
-            double areaOfDarionsRoom = FindArea(69.2, 420);
-            double volumeOfMyRoom = FindVolume(33.2, 56.1, 22.4);
-
-            string[] ourNumbers = new[] { "123-123-1234", "321-321-4321", "sup dude" };
-            
-            for (int i = 0; i < ourNumbers.Length; i++)
-            {
-                if (IsValidPhoneNumber(ourNumbers[i]))
-                {
-                    Console.WriteLine("Hit me up dude");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Phone Number");
-                }
-            }
-
-            
-
         }
 
         static double FindVolume(double length, double width, double height)
@@ -56,5 +80,60 @@ namespace Functions
             bool isValidPhoneNumber = pattern.IsMatch(phoneNumber);
             return isValidPhoneNumber;
         }
+
+        static void PrintHello()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine($"Hello, your message is: {message}");
+        }
+
+        static void PrintMessage(string messageOne, string messageTwo)
+        {
+            Console.WriteLine($"Hello, your first message is: {messageOne}");
+            Console.WriteLine($"Hello, your second message is: {messageTwo}");
+        }
+
+
+        static int GetValidatedUserInt()
+        {
+            // 1. Get user input
+            // 2. TRY to parse user input into int
+            // 3. if valid input return input
+            // 4. repeats step 1 - 3
+            
+            while (true)
+            {
+                Console.WriteLine("Please enter an integer");
+                string userInput = Console.ReadLine();
+                
+                if (int.TryParse(userInput, out int result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, please enter a valid integer");
+                }
+            }
+        }
+
+        static bool TryParsePhoneNumber(string input, out string phoneNumber)
+        {
+            if (IsValidPhoneNumber(input))
+            {
+                phoneNumber = input;
+                return true;
+            } 
+            else
+            {
+                phoneNumber = string.Empty;
+                return false;
+            }
+        }
+        
     }
 }
