c7097310bebf4267bc03bbc108021286bfee3ea0
Jackson, James <jamesjackson@quickenloans.com>
2020-02-03 20:17:35 -0500

    added dictionaries and hashsets

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -71,6 +71,108 @@ namespace Collections
             WriteToConsole<bool, List<string>>(true, new List<string>());
 
             List<List<List<List<List<List<List<List<string>>>>>>>> insanity = new List<List<List<List<List<List<List<List<string>>>>>>>>();
+
+            // Hash Set
+
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            string nameOne = "Darion";
+            string nameTwo = "Darion";
+            string nameThree = "Jamesrion";
+
+            uniqueNames.Add(nameOne);
+            uniqueNames.Add(nameTwo);
+            uniqueNames.Add(nameThree);
+
+            foreach (string uniqueName in uniqueNames)
+            {
+                Console.WriteLine(uniqueName);
+            }
+
+            // Dicionary 
+
+            List<string> dayTimeCSharpStudents = new List<string>()
+            {
+                "Larry",
+                "Barry",
+                "Goldmans Sacks"
+            };
+
+            List<string> afterHoursCSharpStudents = new List<string>()
+            {
+                "Joe",
+                "Moe",
+                "Floe"
+            };
+
+            List<string> afterHoursCHarpStudents = new List<string>()
+            {
+                "Alejandra",
+                "Jonathan",
+                "Tim"
+            };
+
+            int superDopeAssVariable = 2;
+
+            string thisISADifferentDopeAssVariable = nameof(superDopeAssVariable);
+
+
+            Dictionary<string, List<string>> grandCircusClasses = new Dictionary<string, List<string>>();
+
+            grandCircusClasses[nameof(dayTimeCSharpStudents)] = dayTimeCSharpStudents;
+            grandCircusClasses[nameof(afterHoursCSharpStudents)] = afterHoursCHarpStudents;
+            grandCircusClasses[nameof(afterHoursCHarpStudents)] = afterHoursCHarpStudents;
+
+            List<string> someStudentsIGuess = grandCircusClasses[nameof(afterHoursCHarpStudents)];
+
+
+            // more dictionary stuff
+
+            Dictionary<string, int> staffAge = new Dictionary<string, int>();
+
+            staffAge["James"] = 31;
+            staffAge["Darion"] = 30;
+            staffAge["Drew"] = 28;
+
+            if (IsCoolEnoughToPartyBasedOnAge("James", staffAge))
+            {
+                Console.WriteLine("you still got it dude, don't let your class bring you down.");
+            }
+            else
+            {
+                Console.WriteLine("get your bitch ass outta here");
+            }
+
+            if (IsCoolEnoughToPartyBasedOnAge("Darion", staffAge))
+            {
+                Console.WriteLine("we were expecting you");
+            }
+            else
+            {
+                Console.WriteLine("we'll make an exception to the rule");
+            }
+
+
+            staffAge["Drew"] = 13;
+
+            bool isPeterHere = staffAge.ContainsKey("Peter");
+
+            if (staffAge.TryGetValue("Peter", out int petersAge))
+            {
+                Console.WriteLine("I see you're age Peter, it's not secret heh heh heh");
+            }
+            else
+            {
+                Console.WriteLine("I can't get a read on this guy...");
+            }
+
+            int drewsAge = staffAge["Drew"];
+        }
+
+        static bool IsCoolEnoughToPartyBasedOnAge(string name, Dictionary<string, int> staffAge)
+        {
+            bool isCoolEnough = staffAge[name] <= 30;
+            return isCoolEnough;
         }
 
         static void WriteToConsole<T, U>(T message, U otherMessage)
