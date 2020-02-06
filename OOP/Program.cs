1053e936b91191bf1f6417d256edb8f935f6f6d9
Jackson, James <jamesjackson@quickenloans.com>
2020-02-05 20:04:15 -0500

    oop so far

---
+++ OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            WinterHat winterHat = new WinterHat();
+
+            winterHat.Color = "black";
+            winterHat.Size = 2;
+
+            string color = winterHat.Color;
+
+            // string thrownHat = winterHat.ThrowAHat();
+
+            Console.WriteLine(winterHat.Color);
+            Console.WriteLine(winterHat.Size);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP/WinterHat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public class WinterHat
+    {
+
+
+        private string _color;
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+
+            set
+            {
+                _color = value;
+            }
+        }
+
+
+        
+        public double Size { get; set; } // Auto Property
+
+
+        public void WearHat()
+        {
+            // some code to wear hat.
+        }
+
+        private string ThrowAHat()
+        {
+            return "I just threw a hat";
+        }
+    }
+}
