cb278f5cd5d2c4be7ab5fae600658629869bf681
jakyle <jjackson360@gmail.com>
2020-03-19 21:13:56 -0400

    flex and css stuff

---
+++ HTML-intro/main.js
@@ -0,0 +1,12 @@
+var name = "James";
+
+function doTheThing() {
+    alert("Hey, this is a thing!!!");
+    console.log("Am I working");
+}
+
+
+
+const title = document.getElementById("title");
+
+const listItems = document.getElementsByClassName("first-list-item");
