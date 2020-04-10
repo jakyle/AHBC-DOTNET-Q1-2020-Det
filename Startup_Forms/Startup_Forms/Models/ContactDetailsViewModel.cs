403dcb81f1d217b622105ceb25cde88b92d982ba
Jakyle <jjackson360@gmail.com>
2020-04-09 21:05:02 -0400

    validation

---
+++ Startup_Forms/Startup_Forms/Models/ContactDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Startup_Forms.Models
+{
+    public class ContactDetailsViewModel
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
--- Startup_Forms/Startup_Forms/Models/ContactViewModel.cs
+++ Startup_Forms/Startup_Forms/Models/ContactViewModel.cs
@@ -8,8 +8,18 @@ namespace Startup_Forms.Models
 {
     public class ContactViewModel 
     {
+        [Required]
+        [StringLength(50, ErrorMessage = "Invalid name ya shmuck!", MinimumLength = 2)]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "that email is no good here sir")]
         public string EmailAddress { get; set; }
+
+        [Required]
+        [Phone(ErrorMessage = "not a phone number...")]
         public string PhoneNumber { get; set; }
+
+        public string ErrorMessage { get; set; }
     }
 }
